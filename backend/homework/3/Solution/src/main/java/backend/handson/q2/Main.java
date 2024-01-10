5bcf88040b3548c74ac8abc0cf97c9fcdb571772
prithvi206 <prithviraj2062002@gmail.com>
2024-01-10 21:26:46 +0530

    resolved build script error

--- backend/homework/3/Solution/src/main/java/backend/handson/q2/Main.java
+++ backend/homework/3/Solution/src/main/java/backend/handson/q2/Main.java
@@ -8,22 +8,22 @@ public class Main {
     public static void main(String[] args){
         double billedAmount = 10000.0;
         Patient patient = new Patient();
-        LogBack.Outputlogger("Question 1:","INFO");
+        LogBack.outputlogger("Question 1:","INFO");
         patient.setFirstName("arun");
         patient.setLastName("kumar");
         String formattedString = String.format("Patient: %s %s", patient.getFirstName(),patient.getLastName());
-        LogBack.Outputlogger(formattedString,"INFO");
+        LogBack.outputlogger(formattedString,"INFO");
         HealthInsurancePlan platinumPlan = new PlatinumPlan();
         patient.setInsurancPlan(platinumPlan);
-        LogBack.Outputlogger("Question 2:","INFO");
+        LogBack.outputlogger("Question 2:","INFO");
         double[] paymentList = Billing.computePaymentAmount(patient, billedAmount);
-        LogBack.Outputlogger(Double.toString(paymentList[0]),"INFO");
-        LogBack.Outputlogger(Double.toString(paymentList[1]),"INFO");
-        LogBack.Outputlogger("Question 3:","INFO");
+        LogBack.outputlogger(Double.toString(paymentList[0]),"INFO");
+        LogBack.outputlogger(Double.toString(paymentList[1]),"INFO");
+        LogBack.outputlogger("Question 3:","INFO");
         Doctor doctor = new Doctor();
         doctor.setSalary(10000.0);
-        LogBack.Outputlogger(Double.toString(doctor.getSalary()),"INFO");
+        LogBack.outputlogger(Double.toString(doctor.getSalary()),"INFO");
         double monthlyPremium = platinumPlan.computeMonthlyPremium(doctor.getSalary());
-        LogBack.Outputlogger(Double.toString(monthlyPremium),"INFO");
+        LogBack.outputlogger(Double.toString(monthlyPremium),"INFO");
     }
 }
