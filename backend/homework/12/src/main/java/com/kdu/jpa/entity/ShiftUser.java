f03b1ba8a9afb410a01a7e63598b2ca92325d8d9
prithvi206 <prithviraj2062002@gmail.com>
2024-02-01 16:18:25 +0530

    sonar fix

--- backend/homework/12/src/main/java/com/kdu/jpa/entity/ShiftUser.java
+++ backend/homework/12/src/main/java/com/kdu/jpa/entity/ShiftUser.java
@@ -18,13 +18,13 @@ public class ShiftUser {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(name = "shift_id")
+    @ManyToOne(fetch = FetchType.EAGER)
     private Shift shiftId;
 
-    @Column(name = "user_id")
+    @ManyToOne(fetch = FetchType.EAGER)
     private User userId;
 
-    @Column(name = "tenant_id")
+    @ManyToOne(fetch = FetchType.EAGER)
     private Tenant tenantId;
 
 }
