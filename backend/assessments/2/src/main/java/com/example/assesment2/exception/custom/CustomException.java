7386aea482d3be91a3515d4f3ceb4154526397d9
prithvi206 <prithviraj2062002@gmail.com>
2024-02-13 02:42:38 +0530

    deleted unwanted files

--- backend/assessments/2/src/main/java/com/example/assesment2/exception/custom/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.example.assesment2.exception.custom;
-
-public class CustomException extends IndexOutOfBoundsException{
-    public CustomException(String message){
-        super(message);
-    }
-}
